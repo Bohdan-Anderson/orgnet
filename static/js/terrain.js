a61762221c966207aa1c08174895a3a994c58be5
Andrei Vasili <andreiv@gmail.com>
2012-08-15 17:21:17 -0400

    asdfads

--- static/js/terrain.js
+++ static/js/terrain.js
@@ -358,7 +358,7 @@
       zp = pointContainer[iter].z;
       
 	  //speed = 1 * (100/Math.abs(xp));
-      particles[iterLine].rotation.z += 2;
+      particles[iterLine].rotation.z = zp/100;
       
       for (var i = 1; i < (particles[iterLine].geometry.vertices.length + 1); i++) {
         fallOff = zp * Math.exp(-1 * (((i - xp) * (i - xp)) / 256));
